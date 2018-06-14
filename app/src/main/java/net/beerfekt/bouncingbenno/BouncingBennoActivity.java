da8943ec13905c8a8fb98449f66a466a2036ce1e
Wolf2323 <gabrielpatrikurban@gmail.com>
2018-06-14 18:26:36 +0200

    now in fullscreen

--- app/src/main/java/net/beerfekt/bouncingbenno/BouncingBennoActivity.java
+++ app/src/main/java/net/beerfekt/bouncingbenno/BouncingBennoActivity.java
@@ -2,6 +2,7 @@ package net.beerfekt.bouncingbenno;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class BouncingBennoActivity extends Activity {
@@ -9,7 +10,8 @@ public class BouncingBennoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
     }
 }
